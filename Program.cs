7bc47906671df1c0933bbfed3fafb6695f7c5556
Frank Skare <frank.skare.de@gmail.com>
2021-07-22 11:13:19 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿
+using System;
+using System.Diagnostics;
+using System.Linq;
+
+namespace run_hidden
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (Process p = new Process())
+            {
+                p.StartInfo.UseShellExecute = false;
+                p.StartInfo.CreateNoWindow = true;
+                p.StartInfo.FileName = args[0];
+
+                args = args.Skip(1).ToArray();
+
+                for (int i = 0; i < args.Length; i++)
+                    if (args[i].Contains(" "))
+                        args[i] = "\"" + args[i] + "\"";
+
+                p.StartInfo.Arguments = string.Join(" ", args);
+                p.Start();
+                p.WaitForExit();
+                Environment.ExitCode = p.ExitCode;
+            }
+        }
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,9 @@
+
+## About
+
+run-hidden is a simple command line app that allows to run
+console apps like powershell with hidden console window.
+
+## Example command line
+
+run-hidden powershell -command calc.exe
